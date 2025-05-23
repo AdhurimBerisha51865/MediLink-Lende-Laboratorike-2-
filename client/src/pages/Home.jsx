abef9248efe3d0eae0a6b2e3820858acdcce9b10
AdhurimBerisha51865 <ab51865@ubt-uni.net>
2025-05-23 15:03:44 +0200

    Banner Component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import Header from "../components/Header";
 import SpecialtyMenu from "../components/SpecialtyMenu";
 import TopDoctors from "../components/TopDoctors";
+import Banner from "../components/Banner";
 
 const Home = () => {
   return (
     <div>
       <Header />
-       <SpecialtyMenu />
-       <TopDoctors />
+      <SpecialtyMenu />
+      <TopDoctors />
+      <Banner />
     </div>
   );
 };
