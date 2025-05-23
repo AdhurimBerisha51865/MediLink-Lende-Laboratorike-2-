158fe4bd1641cb34544f0a0265bafb32afca2226
gzimbekteshi <gb51876@ubt-uni.net>
2025-05-23 14:45:58 +0200

    SpecialtyMenu Component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
         </div>
         <a
           className="flex items-center gap-2 bg-white px-8 py-3 rounded-full text-gray-600 text-sm m-auto md:m-0 hover:scale-105 transition-all duration-300"
-          href="#speciality"
+         href="#specialty"
         >
           Book appointment{" "}
           <img className="w-3" src={assets.arrow_icon} alt="" />
