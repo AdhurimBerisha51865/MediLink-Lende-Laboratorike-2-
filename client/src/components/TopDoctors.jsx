b96051022221ba349cbaa74653eb78d38ed85ffe
AdhurimBerisha51865 <ab51865@ubt-uni.net>
2025-05-23 15:02:44 +0200

    AppContext

--- client/src/components/TopDoctors.jsx
+++ client/src/components/TopDoctors.jsx
@@ -1,8 +1,10 @@
-import { doctors } from "../assets/assets2";
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { AppContext } from "../context/AppContext";
 
 const TopDoctors = () => {
   const navigate = useNavigate();
+  const { doctors } = useContext(AppContext);
 
   return (
     <div className="flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10">
@@ -29,7 +31,13 @@ const TopDoctors = () => {
           </div>
         ))}
       </div>
-      <button className="bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10">
+      <button
+        onClick={() => {
+          navigate("/doctors");
+          scrollTo(0, 0);
+        }}
+        className="bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10"
+      >
         More
       </button>
     </div>
---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,14 @@
+import { createContext } from "react";
+import { doctors } from "../assets/assets2";
+
+export const AppContext = createContext();
+
+const AppContextProvider = ({ children }) => {
+  const value = {
+    doctors,
+  };
+
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
+
+export default AppContextProvider;
