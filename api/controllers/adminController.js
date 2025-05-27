3548a1ba0f7d93f7c6a079dc0739b1b1fe1cba70
gzimbekteshi <gb51876@ubt-uni.net>
2025-05-27 15:07:10 +0200

    Multer setup, controllers and routes start

---
+++ api/controllers/adminController.js
@@ -0,0 +1,21 @@
+// API for adding doctor
+const addDoctor = async (req, res) => {
+  try {
+    const {
+      name,
+      email,
+      password,
+      specialty,
+      degree,
+      experience,
+      about,
+      fees,
+      address,
+    } = req.body;
+    const imageFile = req.file;
+
+    // checking for all data to add doctor
+  } catch (error) {}
+};
+
+export { addDoctor };
