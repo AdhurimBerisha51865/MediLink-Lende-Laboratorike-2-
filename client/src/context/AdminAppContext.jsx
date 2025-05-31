f15b8826309cdac5c4b7c4e7907d504e9281bf31
AdhurimBerisha51865 <ab51865@ubt-uni.net>
2025-05-31 19:04:16 +0200

    AllAppointments Admin Page

--- client/src/context/AdminAppContext.jsx
+++ client/src/context/AdminAppContext.jsx
@@ -3,7 +3,42 @@ import { createContext } from "react";
 export const AdminAppContext = createContext();
 
 const AdminAppContextProvider = ({ children }) => {
-  const value = {};
+  const currency = "€";
+
+  const calculateAge = (dob) => {
+    const today = new Date();
+    const birthDate = new Date(dob);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    return age;
+  };
+
+  const months = [
+    "",
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  const slotDateFormat = (slotDate) => {
+    const dateArray = slotDate.split("_");
+    return (
+      dateArray[0] + " " + months[Number(dateArray[1])] + " " + dateArray[2]
+    );
+  };
+
+  const value = {
+    calculateAge,
+    slotDateFormat,
+    currency,
+  };
 
   return (
     <AdminAppContext.Provider value={value}>
